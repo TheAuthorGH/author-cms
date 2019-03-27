aeb0acafb2d159ce14bcef004a5cc3e27bec576b
Luis Lau <theauthorgm@gmail.com>
2019-03-27 11:15:05 -0500

    Handle UnauthorizedError properly

--- api/routes.js
+++ api/routes.js
@@ -8,4 +8,10 @@ module.exports = (app) => {
   apiRouter.use('/auth', require('./api-auth'));
 
   app.use('/api', apiRouter);
+
+  app.use((err, req, res, next) => {
+    if(err.name === 'UnauthorizedError') {
+      res.status(401).end();
+    }
+  });
 };
