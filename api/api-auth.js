0ffa53a0a5e1f71463ec4a571a60a5ac58fdd85e
Luis Lau <theauthorgm@gmail.com>
2019-03-29 09:27:37 -0500

    Implement auth refresh endpoint

--- api/api-auth.js
+++ api/api-auth.js
@@ -1,11 +1,21 @@
-const router = require('express').Router();
 
 const jsonParser = require('body-parser').json();
+const {localAuth, requireAuth} = require('./auth');
+const UserModel = require('../models/model-users');
 
-const {localAuth} = require('./auth');
+const router = require('express').Router();
 
 router.post('/', [jsonParser, localAuth], (req, res) => {
   res.json(req.user.createJwt());
+});
+
+router.post('/refresh', [requireAuth], async (req, res) => {
+  const user = await UserModel.findById(req.user.id);
+  if(user) {
+    res.json(user.createJwt());
+  } else {
+    res.status(401).end();
+  }
 });
 
 module.exports = router;
