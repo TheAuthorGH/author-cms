57ce8ab4be5ffd1692e5a96a179f18d135bd7b91
Luis Lau <theauthorgm@gmail.com>
2019-03-28 20:40:34 -0500

    Replace bcrypt with bcryptjs

--- models/model-users.js
+++ models/model-users.js
@@ -1,7 +1,7 @@
 const config = require('../config');
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
