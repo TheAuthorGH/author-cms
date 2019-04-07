4fa851d6359c754f0c59dc93dc70f562ba0a2bce
Luis Lau <theauthorgm@gmail.com>
2019-04-07 18:00:06 -0500

    Cleanup code

--- public/store.js
+++ public/store.js
@@ -5,13 +5,13 @@ window.Vuex = Vuex;
 Vue.use(Vuex);
 
 const requireModule = require.context('./stores', false, /[^\.js]$/);
-const moduleKeys = _.map(requireModule.keys(), _.kebabCase);
+const moduleKeys = requireModule.keys().map(_.kebabCase);
 
 export default new Vuex.Store({
   modules: {
     ..._.zipObject(
-      moduleKeys,
-      _.map(moduleKeys, key => requireModule('./' + key).default)
+      moduleKeys.map(key => key.slice(6)),
+      moduleKeys.map(key => requireModule('./' + key).default)
     )
   },
   state: {
@@ -25,7 +25,7 @@ export default new Vuex.Store({
   actions: {
     async init(context) {
       for(let module of moduleKeys) {
-        await context.dispatch(`${module}/init`);
+        await context.dispatch(`${module.slice(6)}/init`);
       }
       context.commit('setGlobalLoading', false);
     }
